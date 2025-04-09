74d48e11632fec6f821c35a9e555d69c48df9b84
Ryan Duffy <ether.factor@gmail.com>
2025-04-08 22:03:18 -0500

    Expand mock functionality

--- src/core/client/odata-client.mock.ts
+++ src/core/client/odata-client.mock.ts
@@ -1,3 +1,4 @@
+import { SafeAny } from "../../utils/types";
 import { EntitySetBuilderAddKey } from "../entity/client/builder/entity-set-client-builder";
 import { EntitySetBuilderMock } from "../entity/client/builder/entity-set-client-builder.mock";
 import { ODataClient } from "./odata-client";
@@ -5,6 +6,43 @@ import { ODataClient } from "./odata-client";
 export interface MockODataClientOptions {
   getEntitySet(name: string): Record<string, object>;
   addIdToEntity: Record<string, (entity: any) => string>;
+}
+
+export interface NewMockODataClientOptions {
+  entitySets: {
+    [name: string]: {
+      data: () => SafeAny[];
+      id: string | string[];
+      idGenerator: () => SafeAny | SafeAny[];
+      onCreate?: (entity: SafeAny) => void;
+      onUpdate?: (entity: SafeAny) => void;
+      onDelete?: (entity: SafeAny) => void;
+      navigations?: {
+        [property: string]: {
+          targetEntitySet: string;
+          type: "collection" | "single";
+          onAdd?: (sourceEntity: SafeAny, targetEntity: SafeAny) => void;
+          onRemove?: (sourceEntity: SafeAny, targetEntity: SafeAny) => void;
+          onSet?: (sourceEntity: SafeAny, targetEntity: SafeAny) => void;
+          onUnset?: (sourceEntity: SafeAny, targetEntity: SafeAny) => void;
+        }
+      };
+    }
+  };
+  actions: {
+    [name: string]: {
+      entitySet?: string;
+      handler: ((parameters: SafeAny) => SafeAny) |
+        ((entityKey: SafeAny, parameters: SafeAny) => SafeAny);
+    }
+  };
+  functions: {
+    [name: string]: {
+      entitySet?: string;
+      handler: ((parameters: SafeAny) => SafeAny) |
+        ((entityKey: SafeAny, parameters: SafeAny) => SafeAny);
+    }
+  };
 }
 
 export class MockODataClient extends ODataClient {
