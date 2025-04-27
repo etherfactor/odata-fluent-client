9010b4b8fcd86fba51f1ae28f9d3e644e7466590
Ryan Duffy <ether.factor@gmail.com>
2025-04-26 22:25:19 -0500

    Also collect junit results

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["<rootDir>/dist/"],
+  reporters: [
+    "default",
+    ["jest-junit", {
+      outputDirectory: 'test-results',
+      outputName: 'TESTS-all.xml'
+    }]
+  ]
 };
